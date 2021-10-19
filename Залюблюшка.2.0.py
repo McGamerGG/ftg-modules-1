3f8501fb4998384115394a3d5242f3369ba8aeff
McGamerGG <70810032+McGamerGG@users.noreply.github.com>
2021-10-19 12:44:41 +0300

    Залюблюшка 2.0

--- Залюблюшка.2.0.py
+++ Залюблюшка.2.0.py
@@ -2,21 +2,21 @@ from asyncio import sleep
 from .. import loader, utils
 
 def register(cb):
-	cb(ЗаёбушкаMod())
+	cb(ЗалюблюшкаMod())
 	
-class ЗаёбушкаMod(loader.Module):
-	"""Заебет любого"""
-	strings = {'name': 'Заёбушка'}
+class ЗалюблюшкаMod(loader.Module):
+	"""Залюбит любого"""
+	strings = {'name': 'Залюблюшка'}
 	def __init__(self):
 		self.name = self.strings['name']
 		self._me = None
 		self._ratelimit = []
 		
-	async def заебуcmd(self, message):
-		""".заебу <колличество> <реплай на того, кого заебать>"""
+	async def залюблюcmd(self, message):
+		""".залюблю <колличество> <реплай на того, кого залюбить>"""
 		reply = await message.get_reply_message()
 		if not reply:
-			await message.edit("<b>А кого заёбывать-то?</b>")
+			await message.edit("<b>А кого залюбливать-то?</b>")
 			return
 		id = reply.sender_id
 		args = utils.get_args(message)
@@ -28,10 +28,10 @@ class ЗаёбушкаMod(loader.Module):
 				else:
 					count = int(args[0])
 		txt = '<a href="tg://user?id={}">{}</a>'
-		await message.edit(txt.format(id, "Я тебя заебу!"))
+		await message.edit(txt.format(id, "Я тебя залюблю!"))
 		for _ in range(count):
 			await sleep(0.3)
-			msg = await message.client.send_message(message.to_id, txt.format(id, "Заёбушка:3"), reply_to=message)
+			msg = await message.client.send_message(message.to_id, txt.format(id, "Я тебя люблю:3"), reply_to=message)
 			if not msg.is_reply:
 				await msg.edit("<b>Остановлено!</b>")
 				break
